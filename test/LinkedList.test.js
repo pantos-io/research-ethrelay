1208e862efa729c0ac6f22d5a76d4b19ae5cf412
Marten Sigwart <marten@okamaja.de>
2019-07-12 15:22:03 +0200

    add test to check lockedUntil period of a block

--- test/LinkedList.test.js
+++ test/LinkedList.test.js
@@ -2,12 +2,13 @@ const Web3 = require("web3");
 const RLP = require('rlp');
 const { BN, balance, ether, expectRevert, time } = require('openzeppelin-test-helpers');
 
-
 const LinkedList = artifacts.require('./LinkedList');
-
-const genesisRLP = '0xf90217a0f325431224239d833b7d870b4d6d7fd2e6ab4b80857022daa012a9a08277e09fa01dcc4de8dec75d7aab85b567b6ccd41ad312451b948a7413f0a142fd40d493479406b8c5883ec71bc3f4b332081519f23834c8706ea00ceb1811de623435b9e6e35e75b2faa0e732d364b2a3781bc6e9d6b5212b860ba056e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421a056e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421b90100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000008707896d68b38322837b5c1d837a120080845d1ddeaa99d883010817846765746888676f312e31302e34856c696e7578a04db51f8d4a4200dbdcbf0bd0c7bd8c104dfcfa61fb5ba7ef767183f86b1d57d588c23b0a7000b8c2c7';
-
 const { expect } = require('chai');
+
+const GENESIS_RLP = '0xf90217a0f325431224239d833b7d870b4d6d7fd2e6ab4b80857022daa012a9a08277e09fa01dcc4de8dec75d7aab85b567b6ccd41ad312451b948a7413f0a142fd40d493479406b8c5883ec71bc3f4b332081519f23834c8706ea00ceb1811de623435b9e6e35e75b2faa0e732d364b2a3781bc6e9d6b5212b860ba056e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421a056e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421b90100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000008707896d68b38322837b5c1d837a120080845d1ddeaa99d883010817846765746888676f312e31302e34856c696e7578a04db51f8d4a4200dbdcbf0bd0c7bd8c104dfcfa61fb5ba7ef767183f86b1d57d588c23b0a7000b8c2c7';
+const ZERO_BLOCK = '0x0000000000000000000000000000000000000000000000000000000000000000';
+const LOCK_PERIOD = time.duration.minutes(5);
+
 
 contract('LinkedList', async (accounts) => {
 
@@ -16,14 +17,17 @@ contract('LinkedList', async (accounts) => {
 
     before(async () => {
         sourceWeb3 = new Web3("https://mainnet.infura.io");
+        // Advance to the next block to correctly read time in the solidity "now" function interpreted by ganache
+        await time.advanceBlock();
     });
 
     beforeEach(async () => {
-        testimonium = await LinkedList.new(web3.utils.hexToBytes(genesisRLP));
+        testimonium = await LinkedList.new(web3.utils.hexToBytes(GENESIS_RLP));
     });
 
 
     it("should initialise correctly", async () => {
+        const initTime = await time.latest();
         const expectedGenesis = await sourceWeb3.eth.getBlock(8084509);
         expectedGenesis.totalDifficulty = expectedGenesis.difficulty;   // our 'genesis' block's total difficulty will just be its difficulty
         const expectedBlocks = [
@@ -31,7 +35,8 @@ contract('LinkedList', async (accounts) => {
                 block: expectedGenesis,
                 orderedIndex: 0,
                 iterableIndex: 0,
-                latestFork: '0x0000000000000000000000000000000000000000000000000000000000000000'
+                latestFork: ZERO_BLOCK,
+                lockedUntil: initTime
             }
         ];
 
@@ -51,16 +56,13 @@ contract('LinkedList', async (accounts) => {
                 block: block1,
                 orderedIndex: 0,
                 iterableIndex: 0,
-                latestFork: '0x0000000000000000000000000000000000000000000000000000000000000000'
+                latestFork: ZERO_BLOCK
             }
         ];
         await submitExpectedBlockHeaders(expectedBlocks);
 
-        const noOfForks = await testimonium.getNoOfForks();
-        const endpointHash = await testimonium.getBlockHashOfEndpoint(0);
-
         expect(await testimonium.getNoOfForks()).to.be.bignumber.equal(new BN(1));
-        expect(endpointHash).to.equal(block1.hash);
+        expect(await testimonium.getBlockHashOfEndpoint(0)).to.equal(block1.hash);
         await checkExpectedBlockHeaders(expectedBlocks);
     });
 
@@ -77,22 +79,20 @@ contract('LinkedList', async (accounts) => {
                 block: block1,
                 orderedIndex: 0,
                 iterableIndex: 0,
-                latestFork: '0x0000000000000000000000000000000000000000000000000000000000000000'
+                latestFork: ZERO_BLOCK
             },
             {
                 block: block2,
                 orderedIndex: 0,
                 iterableIndex: 0,
-                latestFork: '0x0000000000000000000000000000000000000000000000000000000000000000'
+                latestFork: ZERO_BLOCK
             }
         ];
         await submitExpectedBlockHeaders(expectedBlocks);
 
         // Perform checks
-        const noOfForks = await testimonium.getNoOfForks();
-        const endpointHash = await testimonium.getBlockHashOfEndpoint(0);
         expect(await testimonium.getNoOfForks()).to.be.bignumber.equal(new BN(1));
-        expect(endpointHash).to.equal(block2.hash);
+        expect(await testimonium.getBlockHashOfEndpoint(0)).to.equal(block2.hash);
         await checkExpectedBlockHeaders(expectedBlocks);
     });
 
@@ -131,12 +131,9 @@ contract('LinkedList', async (accounts) => {
         await submitExpectedBlockHeaders(expectedBlocks);
 
         // Perform checks
-        const noOfForks = await testimonium.getNoOfForks();
         expect(await testimonium.getNoOfForks()).to.be.bignumber.equal(new BN(2));
-        const endpointHash0 = await testimonium.getBlockHashOfEndpoint(0);
-        const endpointHash1 = await testimonium.getBlockHashOfEndpoint(1);
-        expect(endpointHash0).to.equal(block1.hash);
-        expect(endpointHash1).to.equal(block2.hash);
+        expect(await testimonium.getBlockHashOfEndpoint(0)).to.equal(block1.hash);
+        expect(await testimonium.getBlockHashOfEndpoint(1)).to.equal(block2.hash);
         await checkExpectedBlockHeaders(expectedBlocks);
     });
 
@@ -188,14 +185,10 @@ contract('LinkedList', async (accounts) => {
         await submitExpectedBlockHeaders(expectedBlocks);
 
         // Perform checks
-        const noOfForks = await testimonium.getNoOfForks();
         expect(await testimonium.getNoOfForks()).to.be.bignumber.equal(new BN(3));
-        const endpointHash0 = await testimonium.getBlockHashOfEndpoint(0);
-        const endpointHash1 = await testimonium.getBlockHashOfEndpoint(1);
-        const endpointHash2 = await testimonium.getBlockHashOfEndpoint(2);
-        expect(endpointHash0).to.equal(block1.hash);
-        expect(endpointHash1).to.equal(block2.hash);
-        expect(endpointHash2).to.equal(block3.hash);
+        expect(await testimonium.getBlockHashOfEndpoint(0)).to.equal(block1.hash);
+        expect(await testimonium.getBlockHashOfEndpoint(1)).to.equal(block2.hash);
+        expect(await testimonium.getBlockHashOfEndpoint(2)).to.equal(block3.hash);
         await checkExpectedBlockHeaders(expectedBlocks);
     });
 
@@ -205,10 +198,35 @@ contract('LinkedList', async (accounts) => {
         await expectRevert(testimonium.submitHeader(rlpHeader), 'Non-existent parent');
     });
 
+    // Test Scenario:
+    //
+    // (0)---(1)
+    //
+    it('should unlock a block at the correct time', async () => {
+        const block1 = await sourceWeb3.eth.getBlock(8084510);
+        const expectedBlocks = [
+            {
+                block: block1,
+                orderedIndex: 0,
+                iterableIndex: 0,
+                latestFork: ZERO_BLOCK
+            }
+        ];
+        await submitExpectedBlockHeaders(expectedBlocks);
+        expect(await testimonium.isUnlocked(block1.hash)).to.equal(false);  // block is locked right after submission
+        await time.increase(LOCK_PERIOD);
+        expect(await testimonium.isUnlocked(block1.hash)).to.equal(false);  // block is locked for duration of lock period
+        await time.increase(time.duration.seconds(1));
+        expect(await testimonium.isUnlocked(block1.hash)).to.equal(true);   // block is unlocked right after lock period has passed
+    });
+
     const submitExpectedBlockHeaders = async (expectedHeaders) => {
-        await expectedHeaders.forEach(async expected => {
+        await asyncForEach(expectedHeaders, async expected => {
             const rlpHeader = createRLPHeader(expected.block);
+            await time.increase(time.duration.seconds(15));
             await testimonium.submitHeader(rlpHeader);
+            const submitTime = await time.latest();
+            expected.lockedUntil = submitTime.add(LOCK_PERIOD);
         });
     };
 
@@ -219,6 +237,7 @@ contract('LinkedList', async (accounts) => {
             expect(actual.latestFork).to.equal(expected.latestFork);
             expect(actual.orderedIndex).to.be.bignumber.equal(new BN(expected.orderedIndex));
             expect(actual.iterableIndex).to.be.bignumber.equal(new BN(expected.iterableIndex));
+            expect(actual.lockedUntil).to.be.bignumber.equal(expected.lockedUntil);
         });
     };
 
@@ -293,5 +312,11 @@ const createRLPHeader = (block) => {
         block.nonce,
     ]);
 };
+
+async function asyncForEach(array, callback) {
+    for (let index = 0; index < array.length; index++) {
+        await callback(array[index], index, array);
+    }
+}
 
 

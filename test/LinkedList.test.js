de8744ebcb5f1f8eae3aaa4a724ee76933d57b05
Marten Sigwart <marten@okamaja.de>
2019-07-12 11:35:47 +0200

    refactor tests

--- test/LinkedList.test.js
+++ test/LinkedList.test.js
@@ -1,12 +1,13 @@
 const Web3 = require("web3");
 const RLP = require('rlp');
+const { BN, balance, ether, expectRevert, time } = require('openzeppelin-test-helpers');
+
 
 const LinkedList = artifacts.require('./LinkedList');
-const BN = web3.utils.BN;
 
 const genesisRLP = '0xf90217a0f325431224239d833b7d870b4d6d7fd2e6ab4b80857022daa012a9a08277e09fa01dcc4de8dec75d7aab85b567b6ccd41ad312451b948a7413f0a142fd40d493479406b8c5883ec71bc3f4b332081519f23834c8706ea00ceb1811de623435b9e6e35e75b2faa0e732d364b2a3781bc6e9d6b5212b860ba056e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421a056e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421b90100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000008707896d68b38322837b5c1d837a120080845d1ddeaa99d883010817846765746888676f312e31302e34856c696e7578a04db51f8d4a4200dbdcbf0bd0c7bd8c104dfcfa61fb5ba7ef767183f86b1d57d588c23b0a7000b8c2c7';
 
-// const {assertRevert} = require('openzeppelin-solidity/test/helpers/assertRevert');
+const { expect } = require('chai');
 
 contract('LinkedList', async (accounts) => {
 
@@ -23,71 +24,199 @@ contract('LinkedList', async (accounts) => {
 
 
     it("should initialise correctly", async () => {
-        const noOfForks = await testimonium.getNoOfForks();
-        const blockHash = await testimonium.getBlockHashOfEndpoint(0);
-        const actualGenesis = toBlock(blockHash, await testimonium.getBlock(blockHash));
         const expectedGenesis = await sourceWeb3.eth.getBlock(8084509);
-
         expectedGenesis.totalDifficulty = expectedGenesis.difficulty;   // our 'genesis' block's total difficulty will just be its difficulty
-        console.log('Genesis Block:', blockHash.valueOf());
-        assertBlocksEqual(actualGenesis, expectedGenesis);
-        assert.equal(noOfForks.valueOf(), 1, "The contract should have one fork initially");
-        assert.equal(actualGenesis.latestFork, '0x0000000000000000000000000000000000000000000000000000000000000000', "The contract should not have a latest fork set initially");
-        assert.equal(actualGenesis.orderedIndex, 0, 'Index in orderedEndpoints array should be 0');
-        assert.equal(actualGenesis.iterableIndex, 0, 'Index in iterableIndex array should be 0');
+        const expectedBlocks = [
+            {
+                block: expectedGenesis,
+                orderedIndex: 0,
+                iterableIndex: 0,
+                latestFork: '0x0000000000000000000000000000000000000000000000000000000000000000'
+            }
+        ];
+
+        expect(await testimonium.getNoOfForks()).to.be.bignumber.equal(new BN(1));
+        expect(await testimonium.getBlockHashOfEndpoint(0)).to.equal(expectedGenesis.hash);
+        await checkExpectedBlockHeaders(expectedBlocks);
     });
 
+    // Test Scenario:
+    //
+    // (0)---(1)
+    //
     it('should add a block header correctly on top of the genesis block', async () => {
-        const noOfForks = await testimonium.getNoOfForks();
-        const expected = await sourceWeb3.eth.getBlock(8084510);
-        const rlpHeader = createRLPHeader(expected);
-        await testimonium.submitHeader(rlpHeader);
-        const blockHash = await testimonium.getBlockHashOfEndpoint(0);
-        const actual = toBlock(blockHash, await testimonium.getBlock(blockHash));
+        const block1 = await sourceWeb3.eth.getBlock(8084510);
+        const expectedBlocks = [
+            {
+                block: block1,
+                orderedIndex: 0,
+                iterableIndex: 0,
+                latestFork: '0x0000000000000000000000000000000000000000000000000000000000000000'
+            }
+        ];
+        await submitExpectedBlockHeaders(expectedBlocks);
 
-        assertBlocksEqual(actual, expected);
-        assert.equal(noOfForks.valueOf(), 1, "The contract should still only have one fork");
-        assert.equal(actual.latestFork, '0x0000000000000000000000000000000000000000000000000000000000000000', "The contract should not have a latest fork set initially");
-        assert.equal(actual.orderedIndex, 0, 'Index in orderedEndpoints array should be 0');
-        assert.equal(actual.iterableIndex, 0, 'Index in iterableIndex array should be 0');
+        const noOfForks = await testimonium.getNoOfForks();
+        const endpointHash = await testimonium.getBlockHashOfEndpoint(0);
+
+        expect(await testimonium.getNoOfForks()).to.be.bignumber.equal(new BN(1));
+        expect(endpointHash).to.equal(block1.hash);
+        await checkExpectedBlockHeaders(expectedBlocks);
     });
 
+    // Test Scenario:
+    //
+    // (0)---(1)---(2)
+    //
+    it('should add two block headers correctly on top of the genesis block', async () => {
+        // Create expected chain
+        const block1 = await sourceWeb3.eth.getBlock(8084510);
+        const block2 = await sourceWeb3.eth.getBlock(8084511);
+        const expectedBlocks = [
+            {
+                block: block1,
+                orderedIndex: 0,
+                iterableIndex: 0,
+                latestFork: '0x0000000000000000000000000000000000000000000000000000000000000000'
+            },
+            {
+                block: block2,
+                orderedIndex: 0,
+                iterableIndex: 0,
+                latestFork: '0x0000000000000000000000000000000000000000000000000000000000000000'
+            }
+        ];
+        await submitExpectedBlockHeaders(expectedBlocks);
+
+        // Perform checks
+        const noOfForks = await testimonium.getNoOfForks();
+        const endpointHash = await testimonium.getBlockHashOfEndpoint(0);
+        expect(await testimonium.getNoOfForks()).to.be.bignumber.equal(new BN(1));
+        expect(endpointHash).to.equal(block2.hash);
+        await checkExpectedBlockHeaders(expectedBlocks);
+    });
+
+    // Test Scenario:
+    //
+    //      -(1)
+    //    /
+    // (0)
+    //    \
+    //      -(2)
+    //
     it('should create a fork correctly', async () => {
-        const branch0 = await sourceWeb3.eth.getBlock(8084510);
-        const branch1 = await sourceWeb3.eth.getBlock(8084510);
+        const block1 = await sourceWeb3.eth.getBlock(8084510);
+        const block2 = await sourceWeb3.eth.getBlock(8084510);
 
-        // change data of branch 1
-        branch1.transactionsRoot = branch0.receiptsRoot;
-        branch1.stateRoot = branch0.transactionsRoot;
-        branch1.receiptsRoot = branch0.stateRoot;
-        branch1.hash = web3.utils.keccak256(createRLPHeader(branch1));  // IMPORTANT: recalculate block hash otherwise asserts fails
+        // change data of block 2
+        block2.transactionsRoot = block1.receiptsRoot;
+        block2.stateRoot = block1.transactionsRoot;
+        block2.receiptsRoot = block1.stateRoot;
+        recalculateBlockHash(block2);  // IMPORTANT: recalculate block hash otherwise asserts fails
 
-        // submit both headers
-        const rlpHeader0 = createRLPHeader(branch0);
-        const rlpHeader1 = createRLPHeader(branch1);
-        await testimonium.submitHeader(rlpHeader0);
-        await testimonium.submitHeader(rlpHeader1);
+        const expectedBlocks = [
+            {
+                block: block1,
+                orderedIndex: 0,
+                iterableIndex: 0,
+                latestFork: block1.parentHash
+            },
+            {
+                block: block2,
+                orderedIndex: 1,
+                iterableIndex: 1,
+                latestFork: block2.parentHash
+            }
+        ];
+        await submitExpectedBlockHeaders(expectedBlocks);
 
+        // Perform checks
         const noOfForks = await testimonium.getNoOfForks();
-        assert.equal(noOfForks.valueOf(), 2, "The contract should have two forks");
-
-        const blockHash0 = await testimonium.getBlockHashOfEndpoint(0);
-        const blockHash1 = await testimonium.getBlockHashOfEndpoint(1);
-        const actual0 = toBlock(blockHash0, await testimonium.getBlock(blockHash0));
-        const actual1 = toBlock(blockHash1, await testimonium.getBlock(blockHash1));
-
-        // check first branch
-        assertBlocksEqual(actual0, branch0);
-        assert.equal(actual0.latestFork, actual0.parentHash, "The parent has become the latest fork");
-        assert.equal(actual0.orderedIndex, 0, 'Index in orderedEndpoints array should be 0');
-        assert.equal(actual0.iterableIndex, 0, 'Index in iterableIndex array should be 0');
-
-        // check second branch
-        assertBlocksEqual(actual1, branch1);
-        assert.equal(actual1.latestFork, actual1.parentHash, "The parent has become the latest fork");
-        assert.equal(actual1.orderedIndex, 1, 'Index in orderedEndpoints array should be 1');
-        assert.equal(actual1.iterableIndex, 1, 'Index in iterableIndex array should be 1');
+        expect(await testimonium.getNoOfForks()).to.be.bignumber.equal(new BN(2));
+        const endpointHash0 = await testimonium.getBlockHashOfEndpoint(0);
+        const endpointHash1 = await testimonium.getBlockHashOfEndpoint(1);
+        expect(endpointHash0).to.equal(block1.hash);
+        expect(endpointHash1).to.equal(block2.hash);
+        await checkExpectedBlockHeaders(expectedBlocks);
     });
+
+    // Test Scenario:
+    //
+    //      -(1)
+    //    /
+    // (0)---(2)
+    //    \
+    //      -(3)
+    //
+    it('should create a three-fork correctly', async () => {
+        const block1 = await sourceWeb3.eth.getBlock(8084510);
+        const block2 = await sourceWeb3.eth.getBlock(8084510);
+        const block3 = await sourceWeb3.eth.getBlock(8084510);
+
+        // change data of block 2
+        block2.transactionsRoot = block1.receiptsRoot;
+        block2.stateRoot = block1.transactionsRoot;
+        block2.receiptsRoot = block1.stateRoot;
+        recalculateBlockHash(block2);  // IMPORTANT: recalculate block hash otherwise asserts fails
+
+        // change data of block 3
+        block3.transactionsRoot = block1.stateRoot;
+        block3.stateRoot = block1.receiptsRoot;
+        block3.receiptsRoot = block1.transactionsRoot;
+        recalculateBlockHash(block3);  // IMPORTANT: recalculate block hash otherwise asserts fails
+
+        const expectedBlocks = [
+            {
+                block: block1,
+                orderedIndex: 0,
+                iterableIndex: 0,
+                latestFork: block1.parentHash
+            },
+            {
+                block: block2,
+                orderedIndex: 1,
+                iterableIndex: 1,
+                latestFork: block2.parentHash
+            },
+            {
+                block: block3,
+                orderedIndex: 2,
+                iterableIndex: 2,
+                latestFork: block3.parentHash
+            }
+        ];
+        await submitExpectedBlockHeaders(expectedBlocks);
+
+        // Perform checks
+        const noOfForks = await testimonium.getNoOfForks();
+        expect(await testimonium.getNoOfForks()).to.be.bignumber.equal(new BN(3));
+        const endpointHash0 = await testimonium.getBlockHashOfEndpoint(0);
+        const endpointHash1 = await testimonium.getBlockHashOfEndpoint(1);
+        const endpointHash2 = await testimonium.getBlockHashOfEndpoint(2);
+        expect(endpointHash0).to.equal(block1.hash);
+        expect(endpointHash1).to.equal(block2.hash);
+        expect(endpointHash2).to.equal(block3.hash);
+        await checkExpectedBlockHeaders(expectedBlocks);
+    });
+
+    const submitExpectedBlockHeaders = async (expectedHeaders) => {
+        await expectedHeaders.forEach(async expected => {
+            const rlpHeader = createRLPHeader(expected.block);
+            await testimonium.submitHeader(rlpHeader);
+        });
+    };
+
+    const checkExpectedBlockHeaders = async (expectedHeaders) => {
+        await expectedHeaders.forEach(async expected => {
+            const actual = toBlock(expected.block.hash, await testimonium.getBlock(web3.utils.hexToBytes(expected.block.hash)));
+            assertBlocksEqual(actual, expected.block);
+            expect(actual.latestFork).to.equal(expected.latestFork);
+            expect(actual.orderedIndex).to.be.bignumber.equal(new BN(expected.orderedIndex));
+            expect(actual.iterableIndex).to.be.bignumber.equal(new BN(expected.iterableIndex));
+        });
+    };
+
+});
 
 
 function Block(
@@ -128,11 +257,15 @@ const toBlock = (hash, result) => {
 };
 
 const assertBlocksEqual = (actual, expected) => {
-    assert.equal(actual.hash, expected.hash, 'Block hashes are different');
-    assert.equal(actual.parent, expected.parent, 'Parent hashes are different');
-    assert.equal(actual.number, expected.number, 'Block numbers are different');
+    expect(actual.hash).to.equal(expected.hash);
+    expect(actual.parent).to.equal(expected.parent);
+    expect(actual.number).to.be.bignumber.equal(new BN(expected.number));
     // todo: maybe parse also total difficulty in Testimonium contract, so we don't have to it calculate manually
     // assert.equal(actual.totalDifficulty, expected.totalDifficulty, 'Total difficulties are different');
+};
+
+const recalculateBlockHash = (block) => {
+    block.hash = web3.utils.keccak256(createRLPHeader(block));
 };
 
 const createRLPHeader = (block) => {

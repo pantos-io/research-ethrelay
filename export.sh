11f6bb67003036ae275480d6026d60418729b2c1
Philipp Frauenthaler <p.frauenthaler@infosys.tuwien.ac.at>
2019-08-12 13:29:25 +0200

    export script: create also binary

--- export.sh
+++ export.sh
@@ -2,6 +2,7 @@
 
 # Create ABI file
 solc --abi contracts/Testimonium.sol --overwrite -o ./abi
+solc --bin contracts/Testimonium.sol --overwrite -o ./bin
 
 # Generate Go file and export to testimonium-cli project
-abigen --abi=abi/Testimonium.abi --pkg=testimonium --out=${GOPATH}/src/github.com/pf92/testimonium-cli/testimonium/Testimonium.go
+abigen --bin=bin/Testimonium.bin --abi=abi/Testimonium.abi --pkg=testimonium --out=${GOPATH}/src/github.com/pf92/go-testimonium/testimonium/TestimoniumContract.go

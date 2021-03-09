b555878d1b09af889566f046034e0088268d40c9
muellerthe <mueller.theresa.c@gmail.com>
2021-03-09 16:18:26 +0100

    Changed export.sh according to naming conventions and README instructions

--- export.sh
+++ export.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-if [[ -z "${GOTESTIMONIUM}" ]]; then
-  echo "Warning: environment variable GOTESTIMONIUM not set."
-  echo "    e.g., 'export GOTESTIMONIUM=~/code/../go-testimonium'"
+if [[ -z "${GOETHRELAY}" ]]; then
+  echo "Warning: environment variable GOETHRELAY not set."
+  echo "    e.g., 'export GOETHRELAY=~/code/../go-ethrelay'"
   exit 0
 fi
 
-EXPORT_PATH=${GOTESTIMONIUM:-${GOPATH}/src/github.com/pantos-io/go-testimonium}
+EXPORT_PATH=${GOETHRELAY:-${GOPATH}/src/github.com/pantos-io/go-ethrelay}
 echo "Exporting into ${EXPORT_PATH}..."
 
 # Create ABI file
@@ -15,7 +15,7 @@ solc --bin contracts/Ethash.sol --overwrite -o ./bin --allow-paths *,
 solc --abi contracts/Testimonium.sol --overwrite -o ./abi --allow-paths *,
 solc --bin contracts/Testimonium.sol --overwrite -o ./bin --allow-paths *,
 
-# Generate Go file and export to go-testimonium project
+# Generate Go file and export to go-ethrelay project
 abigen --bin=bin/Ethash.bin --abi=abi/Ethash.abi --pkg=ethash --out=${EXPORT_PATH}/ethereum/ethash/EthashContract.go
 abigen --bin=bin/Testimonium.bin --abi=abi/Testimonium.abi --pkg=testimonium --out=${EXPORT_PATH}/testimonium/TestimoniumContract.go
 

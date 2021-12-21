d49a24251296acac1d156204e282c3e903664250
Max Kobelt <max.kobelt@tuhh.de>
2021-12-21 16:59:28 +0100

    Fixup event listener script
    Accept contract address as command line argument

--- eventlog.js
+++ eventlog.js
@@ -1,12 +1,11 @@
 const Web3 = require('web3');
-const abiTestimonium = require('./build/contracts/LinkedList.json').abi;
-const web3 = new Web3(new Web3.providers.WebsocketProvider('http://localhost:7545'));
-const account = web3.eth.accounts.wallet.add('0x1ab7e4534afb18687b9f68872e0f3d6c750628ed6c26b64ccb1aad01b785164d');
+const abiTestimonium = require('./build/contracts/Testimonium.json').abi;
+const web3 = new Web3(new Web3.providers.WebsocketProvider('http://127.0.0.1:7545'));
 
 readTestimoniumEvents(web3);
 
 function readTestimoniumEvents(web3) {
-    const contractInstance = new web3.eth.Contract(abiTestimonium, "0xBcA729959391a8d64cF8d08C71b26f3d0e25E83F",{});
+    const contractInstance = new web3.eth.Contract(abiTestimonium, process.argv[2],{});
 
     contractInstance.events.allEvents({
         fromBlock: 0
